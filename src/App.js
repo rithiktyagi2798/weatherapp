f5ae7202e581e297150f682f821f870c19762114
rithiktyagi2798 <rithiktyagi6@gmail.com>
2021-02-15 13:16:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+ import React from 'react';
+import './App.css';
+import Tempapp from './app_component/Tempapp';
+
+function App() {
+  return (
+    <div>
+      <Tempapp />
+    </div>
+  );
+}
+
+export default App;
